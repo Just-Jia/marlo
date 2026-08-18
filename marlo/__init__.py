6f5f17023cd551df1bd919914e451982a12a3d2d
Junbo Jia <68803699+Just-Jia@users.noreply.github.com>
2026-08-18 17:12:00 +0800

    Initialize MARLO library

---
+++ marlo/__init__.py
@@ -0,0 +1,24 @@
+"""
+Lumache - Python library for cooks and food lovers.
+"""
+
+__version__ = "0.0.0"
+
+
+class Graph:
+  """
+  Graph
+  """
+  pass
+
+class MARLO:
+  """
+  MARLO: Manifold Approximation, Graph Reduction and Layout Optimization.
+  """
+  
+  def __init__(self, dim=2):
+    self.dim=dim
+
+  def fit(self):
+    return self
+
